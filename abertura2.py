59318dd45d11135a3b364cd731694b12deae647a
Flávio Oliveira <professor.flavio.oliveira@gmail.com>
2021-03-10 23:57:33 -0300

    Create abertura2.py

---
+++ abertura2.py
@@ -0,0 +1,16 @@
+from PIL import Image
+#from utils import in_file
+import os
+
+# abrindo nossa imagem
+image = Image.open(r"data/input/bandeira.jpeg")
+
+px = image.load() 
+cordinate = x, y = 250, 250
+
+# using getpixel method 
+#print(image.getpixel(90,60))
+print (image.getpixel(cordinate)); 
+
+# exibir a imagem
+image.show()
