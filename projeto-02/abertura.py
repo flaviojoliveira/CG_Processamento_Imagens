969ade2590cfa284f2021a4fb64a49d8de1360c7
Flávio Oliveira <73088855+flaviojoliveira@users.noreply.github.com>
2021-03-25 00:48:07 -0300

    update

--- projeto-02/abertura.py
+++ projeto-02/abertura.py
@@ -7,5 +7,3 @@ image = Image.open("data/input/bandeira.jpeg")
 
 # exibir a imagem
 image.show()
-
-docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
