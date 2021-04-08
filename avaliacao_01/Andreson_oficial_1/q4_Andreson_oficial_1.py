c1c1ba9c1a82f3b90c67f3cd2b9dac4b4c1e342d
andreson-gms <62163882+andreson-gms@users.noreply.github.com>
2021-04-07 21:01:02 -0300

    Update q4_Andreson_oficial_1.py

--- avaliacao_01/Andreson_oficial_1/q4_Andreson_oficial_1.py
+++ avaliacao_01/Andreson_oficial_1/q4_Andreson_oficial_1.py
@@ -1,13 +1,16 @@
+#=======================================#
+#       imagem do tipo vetorial         #
+#=======================================#
 import cairo
 
 with cairo.SVGSurface("example.svg", 200, 200) as surface:
     c = cairo.Context(surface)
     c.scale(200,200)
-    c.set_line_width(0.05)
+    c.set_line_width(1)
     c.move_to(1, 1)
     c.line_to(0.5, 0.5)
-    c.move_to(2, 1)
+    c.move_to(2, 2)
     c.line_to(-0.5, -0.5)
 
-    c.set_source_rgb(0, 0, 1)
+    c.set_source_rgb(0.05, 0, 1)
     c.stroke()
