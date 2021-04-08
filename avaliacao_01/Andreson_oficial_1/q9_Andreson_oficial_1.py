d65907b9fc67940d64cd321ef8b5680dfb5566f9
andreson-gms <62163882+andreson-gms@users.noreply.github.com>
2021-04-08 17:41:05 -0300

    Update q9_Andreson_oficial_1.py

--- avaliacao_01/Andreson_oficial_1/q9_Andreson_oficial_1.py
+++ avaliacao_01/Andreson_oficial_1/q9_Andreson_oficial_1.py
@@ -1,15 +1,17 @@
 from PIL import Image
+data = []
 
 image = Image.open("./avaliacao_01/Andreson_oficial_1/q6.jpg")
+image2 = Image.open("./avaliacao_01/Andreson_oficial_1/q6.jpg")
 
 #  # Exibir o formato do arquivo;
-# print(image.format)
+print(image.format)
 #   # Exibir o modo do canal formato de pixel
-# print(image.mode)
+print(image.mode)
 #   # Dimensão da imagem em pixel
-# print(image.size)
+print(image.size)
 #   # Exibir as propriedades da imagem em um array de pixel
-# image2 = Image.fromarray(data)
+image2 = Image.fromarray(data)
 #   # Exibir o array de pixel como imagem
 # image2 = Image.fromarray(data)
 #   # converter o array de pixel em objeto Image Pillow
